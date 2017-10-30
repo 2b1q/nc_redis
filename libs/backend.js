bdb10c5ef2316eb395b101c8e90cbf607f890220
bbq <b-b-q@ya.ru>
2017-10-30 16:31:59 +0300

    refactored backean. Add promises AND 50/50 fail + save fail msg hashes to redis Hospital store

--- libs/backend.js
+++ libs/backend.js
@@ -20,16 +20,61 @@ client.on('error', function (err) {
   process.exit(0);
 });
 
-var get_msg = function(keyid){
-  client.exists(keyid, function(err, reply) {
-    if(reply === 1) {
-      //KEY exists. GET hashes object
-      client.hgetall(keyid, function(err, object) {
+// getkey Promise
+function getkey(keyid){
+  console.log('\nGET key: "%s"\n',keyid);
+  return new Promise(function(resolve, reject){
+    client.hgetall(keyid, function(err, object) {
+      if(err) reject(err);
+      if(object) {
         console.log(object);
-        rm_msg(keyid);
-      });
-    } else log.warn('KEY %s already readed from Redis store', keyid);
+        // 50/50 FAIL and store KEYID if FAIL
+        Math.random() > .5
+                    ? resolve(keyid)
+                    : reject('Read hashes from REDIS FAIL.\nExec IIFE and RESAVE hashes to Hospital STORE\n'
+                    +(function(hospital_object){
+                      client.hmset('hospital_'+keyid, hospital_object); // Storing hospital_object to hospital
+                      return 'Storing hospital_object to hospital'})(object))
+        // resolve(keyid); // pass keyid to rmkey(keyid)
+      }
+    });
   });
+}
+
+// rmkey Promise
+function rmkey(keyid){
+  console.log(config.color.cyan+'\nREMOVE key '+config.color.green+'%s'+config.color.cyan+' from Redis store\n', keyid);
+  return new Promise(function(resolve, reject){
+    client.del(keyid, function(err, reply) {
+      // log.warn(config.color.green+'\nRead and Remove KEY '+config.color.yellow+'%s'+config.color.cyan+' Replay: %s',keyid,reply);
+      if(err) reject('remove key from redis FAIL. Error: %s', err)
+      if(reply) resolve(log.warn(config.color.green+'\nRead and Remove KEY '+config.color.yellow+'%s'+config.color.cyan+' Replay: %s',keyid,reply))
+    });
+  });
+}
+
+// get msg with Promise
+var get_msg = function(keyid){
+  new Promise(function(resolve, reject){
+    client.exists(keyid, function(err, reply) {
+      if( err ) reject(err);
+      if( reply === 1 ) resolve(keyid);
+      else reject('KEY '+keyid+' not exists in Redis store');
+    });
+  })
+  .then(getkey)
+  .then(rmkey)
+  .catch(error => log.error(error))
+
+  // client.exists(keyid, function(err, reply) {
+  //   if(reply === 1) {
+  //     //KEY exists. GET hashes object
+  //     client.hgetall(keyid, function(err, object) {
+  //       console.log(object);
+  //       rm_msg(keyid);
+  //     });
+  //   } else log.warn('KEY %s already readed from Redis store', keyid);
+  // });
 }
 
 // remove msg
@@ -43,10 +88,8 @@ var getall_keys = function(){
   client.keys('*', function (err, keys) {
     if(err) return log.error(err);
     for(var i = 0, len = keys.length; i < len; i++) {
-      if(keys[i].length > 10) {
+      if(keys[i].length === 32) { // get (not Hostpital_ and Not generator) keys
         get_msg(keys[i]);
-        // console.log(keys[i]);
-        // return keys[i];
       }
     }
   });
